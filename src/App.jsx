c8828b9dcb1fbb80e2893d4947303621fa5d0733
Jordi, Pastor <jordi.pastor@sky.uk>
2024-06-17 09:25:45 +0100

    Jordi Pastor fixed app.jsx format

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
+import './App.css';
 import ClientManagement from "./client/ClientManagement";
-import './App.css'
 
 const App = () => {
   return (
     <div className="container">
       <h1>Client Management System</h1>
-        <ClientManagement />
+      <ClientManagement />
     </div>
   )
 };
