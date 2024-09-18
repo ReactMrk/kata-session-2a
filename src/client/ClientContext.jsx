065c273416aa5fef2742087dfe5161629fbef9c8
Jordi <jordi.pastor@sky.uk>
2024-09-18 23:20:40 +0200

    Jordi Pastor update init

--- src/client/ClientContext.jsx
+++ src/client/ClientContext.jsx
@@ -3,7 +3,7 @@ import { useState, createContext } from 'react';
 const ClientContext = createContext();
 
 const ClientProvider = ({ children }) => {
-  // Exercise 2 
+  // Context exercise 
 };
 
 export { ClientContext, ClientProvider };
--- src/client/useAlert.js
+++ src/client/useAlert.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useContext } from 'react';
+import { useState, useEffect } from 'react';
 
 const useAlert = ({ clients }) => {
   const [displayAlert, setDisplayAlert] = useState(false);
